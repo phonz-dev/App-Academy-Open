9b021a0e4d9bb0a6cc0c018f03647310d1aa2304
Alphonzo Escolar <phonz.dev@gmail.com>
2021-07-30 15:23:45 +0800

    Finish exercise

---
+++ software_engineering_foundations/algorithmic_strategies_exercise/04_peak_finder.rb
@@ -0,0 +1,27 @@
+# Write a method, peak_finder(arr), that accepts an array of numbers as an arg.
+# The method should return an array containing all of "peaks" of the array.
+# An element is considered a "peak" if it is greater than both it's left and right neighbor.
+# The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
+
+def peak_finder(arr)
+    peaks = []
+    last_el = arr.length - 1
+
+    arr.each_with_index do |num, i|
+        if i == 0 && arr[i] > arr[i + 1]
+            peaks << num
+        elsif i == last_el && arr[i] > arr[i - 1]
+            peaks << num
+        elsif arr[i] > arr[i - 1] && arr[i] > arr[i + 1]
+            peaks << num
+        end
+    end
+
+    peaks
+end
+
+p peak_finder([1, 3, 5, 4])         # => [5]
+p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
+p peak_finder([4, 2, 11, 6, 10])    # => [4, 11, 10]
+p peak_finder([1, 3])               # => [3]
+p peak_finder([3, 1])               # => [3]
