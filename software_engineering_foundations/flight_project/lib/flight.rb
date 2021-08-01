a4d1b77aa9c24c6a5ec97c2e4c38a1f0eb815c9d
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-01 21:05:06 +0800

    Finish flight project

---
+++ software_engineering_foundations/flight_project/lib/flight.rb
@@ -0,0 +1,30 @@
+class Flight
+    attr_reader :passengers
+    def initialize(flight_number, capacity)
+        @flight_number = flight_number
+        @capacity = capacity
+        @passengers = []
+    end
+
+    def full?
+        passengers.count == @capacity
+    end
+
+    def board_passenger(passenger)
+        if passenger.has_flight?(@flight_number) && !full?
+            passengers << passenger
+        end
+    end
+
+    def list_passengers
+        passengers.map { |passenger| passenger.name }
+    end
+
+    def [](index)
+        passengers[index]
+    end
+
+    def <<(passenger)
+        board_passenger(passenger)
+    end
+end
