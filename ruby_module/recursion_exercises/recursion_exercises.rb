cd13101d908c6538fce15a2ec271fecb8c6519a6
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-07 21:15:32 +0800

    Finish binary search

---
+++ ruby_module/recursion_exercises/recursion_exercises.rb
@@ -0,0 +1,96 @@
+require "byebug"
+# Warm up
+# iterative solution
+def range(start_num, end_num)
+    # one line version
+    # (start_num...end_num).to_a
+
+    # version 2
+    exclusive_range = []
+    (start_num...end_num).each do |num|
+        exclusive_range << num
+    end
+    exclusive_range
+end
+
+# recursive solution
+def range(start_num, end_num)
+    return [] if end_num <= start_num
+    [start_num] + range(start_num + 1, end_num)
+end
+
+# Exponentiation version 1
+def exponent(base, power)
+    return 1 if power == 0
+    base * exponent(base, power - 1)
+end
+
+# Exponentiation version 2
+def exponent(base, power)
+    return 1 if power == 0
+    return base if power == 1
+    if power.even?
+        exponent(base, power / 2) * exponent(base, power / 2)
+    else
+        base * exponent(base, (power - 1) / 2) * exponent(base, (power - 1) / 2)
+    end
+end
+
+# Deep dup
+class Array
+    def deep_dup
+        copy = []
+        self.each do |el|
+            copy << (el.is_a?(Array) ? el.deep_dup : el)
+        end
+        copy
+    end
+end
+
+# iterative solution
+def fibonacci(n)
+    return [] if n == 0
+    return [1] if n == 1
+    seq = [1, 1]
+    while seq.length < n
+        seq << seq[-1] + seq[-2]
+    end
+    seq
+end
+
+# recursive solution
+def fibonacci(n)
+    return [] if n == 0
+    return [1] if n == 1
+    return [1, 1] if n == 2
+    seq = fibonacci(n - 1)
+    seq << seq[-1] + seq[-2]
+end
+
+def bsearch(array, target)
+    return nil if array.empty?
+
+    mid = array.length / 2
+    middle_el = array[mid]
+    left_half = array[0...mid]
+    right_half = array[mid + 1..-1]
+    
+    if target == middle_el
+        mid
+    elsif target < middle_el
+        bsearch(left_half, target)
+    else
+        ans = bsearch(right_half, target)
+        ans.nil? ? nil : (mid + 1) + ans
+    end
+end
+
+
+
+
+
+
+
+
+
+
