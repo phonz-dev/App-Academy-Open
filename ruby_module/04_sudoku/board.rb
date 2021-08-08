4a2504757d2447e09881b0cabaffd8e0ce2ced36
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-09 07:47:17 +0800

    Add Board#render

--- ruby_module/04_sudoku/board.rb
+++ ruby_module/04_sudoku/board.rb
@@ -1,6 +1,9 @@
 require_relative "tile"
+require "colorize"
 
 class Board
+    attr_reader :grid
+
     def self.from_file(file)
         board = []
         File.readlines(file, chomp: true).each do |line|
@@ -16,4 +19,13 @@ class Board
     def initialize(grid)
         @grid = grid
     end
+
+    def render
+        puts "\s\s#{(0...grid.size).to_a.join(" ")}"
+        grid.each_with_index do |row, idx|
+            puts "#{idx} #{row.map(&:to_s).join(" ")}"
+        end
+    end
+
 end
+
