30af34fd2cf5b3cd0cadb4fa801c65b840bcd02b
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-09 14:08:35 +0800

    Refactor tile

--- ruby_module/04_sudoku/tile.rb
+++ ruby_module/04_sudoku/tile.rb
@@ -1,10 +1,25 @@
+require "colorize"
+
 class Tile
     attr_reader :value
     def initialize(value)
         @value = value
+        @given = value == 0 ? false : true
+    end
+
+    def given?
+        @given
     end
 
     def to_s
-        value == 0 ? " " : value
+        value == 0 ? " " : value.to_s.colorize(:magenta)
+    end
+
+    def value=(new_value)
+        if given?
+            puts "You cannot change a given value."
+        else
+            @value = new_value.to_s.colorize(:blue)
+        end
     end
 end
