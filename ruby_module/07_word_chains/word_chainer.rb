b4418e6ceaa800b4696bb7700a58669b62c9252d
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-12 19:51:31 +0800

    Refactor #adjacent_words

--- ruby_module/07_word_chains/word_chainer.rb
+++ ruby_module/07_word_chains/word_chainer.rb
@@ -1,16 +1,35 @@
 require 'set'
 
 class WordChainer
-    attr_reader :dictionary
+    attr_reader :dictionary, :current_words, :all_seen_words
 
     def initialize(dictionary_file)
         @dictionary = File.readlines(dictionary_file).map(&:chomp).to_set
     end
 
     def adjacent_words(word)
-        dictionary.select do |adj_word|
-            word.length == adj_word.length &&
-                word[1..-1] == adj_word[1..-1]
+        words = []
+        same_length = same_length_words(word, dictionary)
+
+        same_length.each_with_index do |str, i|
+            (0...word.length).each do |i|
+                word_fragment = slice_char(word, i)
+                str_fragment = slice_char(str, i)
+                
+                if (word_fragment == str_fragment) && word != str
+                    words << str 
+                end
+            end
         end
+
+        words
+    end
+
+    def slice_char(word, index)
+        word[0...index] + word[index + 1..-1]
+    end
+
+    def same_length_words(word, words)
+        words.select { |str| word.length == str.length }
     end
 end
