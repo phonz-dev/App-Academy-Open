e9fc858730fce1a9b9252f6d665747c8662ace40
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-14 15:06:45 +0800

    Initial commit

---
+++ ruby_module/09_minesweeper/tile.rb
@@ -0,0 +1,30 @@
+class Tile
+    attr_reader :bomb, :flag, :reveal, :value
+
+    def initialize(value = "_")
+        @bomb = value == "B" ? true : false
+        @flag = value == "F" ? true : false
+        @reveal = false
+        @value = value
+    end
+
+    def bombed?
+        bomb
+    end
+
+    def flagged?
+        flag
+    end
+
+    def revealed?
+        reveal
+    end
+
+    def value=(new_value)
+        @value = new_value
+    end
+
+    def to_s
+        value
+    end
+end
