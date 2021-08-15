01aa8900839e0a4929c3902b0eafd38346cf11fa
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-15 13:57:46 +0800

    Modify tile methods

--- ruby_module/09_minesweeper/tile.rb
+++ ruby_module/09_minesweeper/tile.rb
@@ -2,10 +2,16 @@ class Tile
     attr_reader :bomb, :flag, :reveal, :value
 
     def initialize(value = "_")
-        @bomb = value == :B ? true : false
-        @flag = value == :F ? true : false
-        @reveal = false
+        @bomb = false
+        @flag = false
+        @reveal = true
         @value = value
+
+        if value == "B"
+            @bomb = true
+        elsif value == "F"
+            @flag = true
+        end
     end
 
     def bombed?
@@ -17,15 +23,18 @@ class Tile
     end
 
     def revealed?
-        @reveal
-    end
-
-    def reveal
-        @reveal = true
+        reveal
     end
 
     def value=(new_value)
         @value = new_value
+
+        case value
+        when "B"
+            @bomb = true
+        when "F"
+            @flag = true
+        end
     end
 
     def to_s
