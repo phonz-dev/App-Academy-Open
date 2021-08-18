a5086d16bcb7e54c239b03c78c44d3d43e57a705
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-18 20:42:10 +0800

    Add #parent=, #add_child, #remove_child

--- ruby_module/10_poly_tree_node/lib/00_tree_node.rb
+++ ruby_module/10_poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    def parent=(new_parent)
+        parent.children.pop unless parent.nil?
+        @parent = new_parent
+        parent.children << self unless parent.nil?
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        unless children.include?(child_node)
+            raise "Node does not exist"
+        end
+        child_node.parent = nil
+    end
 end
