e1add7e151ea1ac95b963c5cb3236b003a7279ce
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-18 11:47:22 +0800

    Create Stack class with its methods #push, #pop, #peek

---
+++ ruby_module/00_homeworks/diy_adts/adts.rb
@@ -0,0 +1,20 @@
+class Stack
+    def initialize
+        @stack = []
+    end
+
+    def push(el)
+        stack << el
+    end
+
+    def pop
+        stack.pop
+    end
+
+    def peek
+        stack.first
+    end
+
+    private
+    attr_reader :stack
+end
