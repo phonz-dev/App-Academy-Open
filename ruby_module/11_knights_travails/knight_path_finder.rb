4f4c483cf6f77d1d9920a9e892e3586c0a3b00e2
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-19 15:44:10 +0800

    Add KnightPathFinder class with ::valid_moves, ::valid_pos? and #initialize methods

---
+++ ruby_module/11_knights_travails/knight_path_finder.rb
@@ -0,0 +1,39 @@
+require_relative "poly_tree_node"
+
+class KnightPathFinder
+    attr_reader :root_node
+
+    POSITION_MAPPER = [
+        [1,2],
+        [-1,2],
+        [1,-2],
+        [-1,-2],
+        [2,1],
+        [2,-1],
+        [-2,1],
+        [-2,-1]
+    ]
+
+    def self.valid_moves(pos)
+        row, col = pos
+        valid_positions = []
+        POSITION_MAPPER.each do |(r1, c1)|
+            new_row = row + r1
+            new_col = col + c1
+            if KnightPathFinder.valid_pos?(new_row, new_col)
+                valid_positions << [new_row, new_col]
+            end
+        end
+        valid_positions
+    end
+
+    def self.valid_pos?(row, col)
+        board_range = (0..7).to_a
+        board_range.include?(row) &&
+            board_range.include?(col)
+    end
+
+    def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+    end
+end
