ebdbb97ace0479df6553107f1a89727f4e251eb4
Alphonzo Escolar <phonz.dev@gmail.com>
2021-08-20 18:19:05 +0800

    Add #move

--- ruby_module/12_tic_tac_toe_ai/lib/super_computer_player.rb
+++ ruby_module/12_tic_tac_toe_ai/lib/super_computer_player.rb
@@ -2,6 +2,21 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    board = TicTacToeNode.new(game.board, mark)
+
+    board.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    board.children.each do |child|
+      unless child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    raise "this is unwinnable"
   end
 end
 
